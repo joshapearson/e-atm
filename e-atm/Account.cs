96de8dff1bc55d4d5e41359866f43e36f0c6f6dd
Conference Computer <djacobo@srca.net>
2016-07-05 13:17:30 -0500

    use a class object

---
+++ e-atm/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_atm
+{
+	public class Account
+	{
+		public string accountName{get;set;}
+		public string accountPin{get;set;}
+		public double amount{get;set;}
+		public int withdrawalNumber{get;set;}
+		public bool UsedAllWithdrawals()
+		{
+			return (withdrawalNumber <= 0);
+		}
+	}
+}
--- e-atm/Program e_atm.cs
+++ e-atm/Program e_atm.cs
@@ -6,99 +6,119 @@ using System.Threading.Tasks;
 
 namespace e_atm
 {
+	class program
+	{
+		static void Main(string[] args)
+		{
+			Account johnDoe = new Account();
+			johnDoe.accountName = "John Doe";
+			johnDoe.accountPin = "3456";
+			johnDoe.amount = 5000;
+			johnDoe.withdrawalNumber = 10;
 
-    class program
-        {
-        static void Main(string[] args)
-        {
+			Account marySmith = new Account();
+			marySmith.accountName = "Mary Smith";
+			marySmith.accountPin = "6789";
+			marySmith.amount = 10000;
+			marySmith.withdrawalNumber = 10;
 
-           
-        bool incorrectPin = true;
-            
-            while(incorrectPin)
-            {
-                Console.WriteLine("please input a pin number");
-                string pinHolder = Console.ReadLine();
-                if (pinHolder == "1234")
-                {
+			Account johnSmith = new Account();
+			johnSmith.accountName = " john Smith";
+			johnSmith.accountPin = "1011";
+			johnSmith.amount = 6500;
+			johnSmith.withdrawalNumber = 10;
 
-                    int amount = 10000, deposit, withdraw;
-                    int menuChoice = 0;                   
-                    incorrectPin = false;
-                    Console.WriteLine("Welcome to your account");
-                    Console.ReadLine();
+			List<Account> accounts = new List<Account>();
+			accounts.Add(johnDoe);
+			accounts.Add(marySmith);
+			accounts.Add(johnSmith);
+			while (true)
+			{
+				Console.WriteLine("please input a pin number");
+				string pinHolder = Console.ReadLine();
+				Account account = accounts.SingleOrDefault(a => a.accountPin == pinHolder);
+				if (account != null)
+				{
+					double amount = account.amount, deposit, withdraw;
+					int menuChoice = 0;					
+					Console.WriteLine("Welcome to your account");
+					Console.ReadLine();
 
-                    while (menuChoice != 4)
-                    { 
-                    Console.WriteLine("Please Select an option from the list");
-                    Console.WriteLine("1. Check your Balance\n");
-                    Console.WriteLine("2. Withdraw Cash\n");
-                    Console.WriteLine("3. Make a Deposit\n");
-                    Console.WriteLine("4. Exit\n");
-                    menuChoice = int.Parse(Console.ReadLine());
+					while (menuChoice != 4)
+					{
+						Console.WriteLine("Please Select an option from the list");
+						Console.WriteLine("1. Check your Balance\n");
+						Console.WriteLine("2. Withdraw Cash\n");
+						Console.WriteLine("3. Make a Deposit\n");
+						Console.WriteLine("4. Exit\n");
+						menuChoice = int.Parse(Console.ReadLine());
 
-                        switch (menuChoice)
-                        {
-                            case 1:
-                                Console.WriteLine("\n Your current Balance in Rs : {0} ", amount);
-                                break;
-                            case 2:
-                                Console.WriteLine("\n please enter withdrawl amount: ");
-                                withdraw = int.Parse(Console.ReadLine());
+						switch (menuChoice)
+						{
+							case 1:
+								Console.WriteLine("\n Your current Balance in $s : {0} ", amount);
+								break;
+							case 2:
+								Console.WriteLine("\n please enter withdrawl amount: ");
+								withdraw = int.Parse(Console.ReadLine());								
 
-                                if (withdraw % 1000 != 0)
-                                {
-                                    Console.WriteLine("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 10");
-                                    Console.WriteLine("transaction maximum is $1000");
-                                }
+								if (withdraw > 1000)
+								{
+									Console.WriteLine("\n amount exceeds withdrawl limit");
+								}
+								else
+								{
+									
+									if(account.UsedAllWithdrawals())
+									{
+										Console.WriteLine("total withdrawals reached");	
 
-                                else if (withdraw > (amount - 1000))
-                                {
-                                    Console.WriteLine("\n amount exceeds withdrawl limit");
-                                }
-                                else
-                                {
-                                    amount = amount - withdraw;
-                                    Console.WriteLine("\n\n Please take your cash");
-                                    Console.WriteLine("\n YOUR CURRENT BALANCE IS {0}", amount);
-                                    for (int i = 0; i < 10; i++)
-                                    {
-                                        if (i <= 10)
-                                            continue;
-                                        if (1 > 10)
-                                            break;
+									}
+									else
+									{
+										account.withdrawalNumber--;
+										Console.WriteLine("You have {0} withdrawals left", account.withdrawalNumber );
+									
+									}
+									
+									amount = amount - withdraw;
+									Console.WriteLine("\n\n Please take your cash");
+									Console.WriteLine("\n YOUR CURRENT BALANCE IS {0}", amount);
+									account.amount = amount;
+									for (int i = 0; i < 10; i++)
+									{
+										if (i <= 10)
+											continue;
+										if (1 > 10)
+											break;
+									}
+								}
+								break;
+							case 3:
+								Console.WriteLine("\n ENTER THE AMOUNT TO DEPOSIT");
+								deposit = int.Parse(Console.ReadLine());
+								amount = amount + deposit;
+								Console.WriteLine("YOUR BALANCE IS {0}", amount);
+								account.amount = amount;
+								break;
+							case 4:
+								Console.WriteLine("\n Thank you for using the new E-ATM!");
+								break;
+							default:
+								Console.WriteLine("\nPlease enter a valid option.");
+								break;
+						}
+					}
+				}
 
-                                    
+				else
+				{
+					
+					Console.WriteLine("Pin not found");
+				}
+			}
+		}
 
-                                
-                                    }
-                                }
-                                break;
-                            case 3:
-                                Console.WriteLine("\n ENTER THE AMOUNT TO DEPOSIT");
-                                deposit = int.Parse(Console.ReadLine());
-                                amount = amount + deposit;
-                                Console.WriteLine("YOUR BALANCE IS {0}", amount);
-                                break;
-                            case 4:
-                                Console.WriteLine("\n Thank you for using the new E-ATM!");
-                                break;
-                            }
-                        }
-                }
-   
-                else
-                {
-                    incorrectPin = true;
-                }
-                
-                
-                    
-             }
-  
-           }
-
-        }
-
-    }
+	}
+}
 
